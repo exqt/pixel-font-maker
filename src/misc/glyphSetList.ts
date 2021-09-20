a2dad16bbf8bb3aa13447ea5a016e37db8becd9a
exqt <exqt_@naver.com>
2021-09-20 13:33:52 +0900

    Add more glyphset - cyrillic, enclosed letters, cjk symbols

--- src/misc/glyphSetList.ts
+++ src/misc/glyphSetList.ts
@@ -4,10 +4,13 @@ import ksx1001SyllablesSet from "./ksx1001";
 const asciiSet = new GlyphSet("ASCII", [[0, 0], [33, 127]]);
 const latinExtendedSet = new GlyphSet("Latin-1 Supplement, Latin Extended-AB", [[0xA1, 0x24F]]);
 const greekSet = new GlyphSet("Greek", [[0x370, 0x3FF]]);
+const cyrillicSet = new GlyphSet("Cyrillic", [[0x400, 0x52F]]);
 const arrowsSet = new GlyphSet("Arrows", [[0x2190, 0x21FF]]);
+const enclosedSet = new GlyphSet("Enclosed Alphanumerics & CJK Letters and Months", [[0x2460, 0x24FF], [0x3200, 0x32FF]]);
 const geometricShapesSet = new GlyphSet("Geometry Shapes", [[0x25A0, 0x25FF]]);
 const boxDrawingBlockElementSet = new GlyphSet("Box drawing & Block Elements", [[0x2500, 0x259F]]);
 const miscellaneousSymbolsSet = new GlyphSet("Miscellaneous Symbols", [[0x2600, 0x26FF]]);
+const CJKSymbolsSet = new GlyphSet("CJK Symbols and Punctuation", [[0x3000, 0x303F]]);
 const CJKUnifiedIdeographs = new GlyphSet("CJK Unified Ideographs", [[0x4E00, 0x9FFF]]);
 const japaneseHiraganaSet = new GlyphSet("Japanese Hiragana", [[0x3040, 0x309F]]);
 const japaneseKatakanaSet = new GlyphSet("Japanese Katakana", [[0x30A0, 0x30FF]]);
@@ -19,10 +22,13 @@ let GlyphSetList = [
   asciiSet,
   latinExtendedSet,
   greekSet,
+  cyrillicSet,
+  enclosedSet,
   arrowsSet,
   geometricShapesSet,
   boxDrawingBlockElementSet,
   miscellaneousSymbolsSet,
+  CJKSymbolsSet,
   CJKUnifiedIdeographs,
   japaneseHiraganaSet,
   japaneseKatakanaSet,
